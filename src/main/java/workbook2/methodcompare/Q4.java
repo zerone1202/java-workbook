954677fd8d9dfa8bec40b7396ffc38a5fd366f92
zerone <bazz0324@naver.com>
2025-08-27 20:58:18 +0900

    "feat: [methodcompare] Q4 구현"

--- src/main/java/workbook2/methodcompare/Q4.java
+++ src/main/java/workbook2/methodcompare/Q4.java
@@ -1,11 +1,23 @@
 package main.java.workbook2.methodcompare;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 class StringUtil {
     static boolean isPalindrome(String s) {
         // TODO: 회문 여부 판단
-        return false;
+        if (s == null || s.length() == 0)
+            return true;
+
+        int a = 0;
+        int b = s.length() - 1;
+        while (a < b) {
+            if (s.charAt(a) != s.charAt(b))
+                return false;
+            a++;
+            b--;
+        }
+        return true;
     }
 }
 
@@ -14,5 +26,6 @@ public class Q4 {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         // TODO: StringUtil.isPalindrome 호출 후 출력
+        System.out.println(StringUtil.isPalindrome(str));
     }
 }
